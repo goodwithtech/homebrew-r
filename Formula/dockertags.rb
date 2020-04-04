a1da6ee1da2a9bc1935dc3c930fa10fac6c57ced
goreleaserbot <goreleaser@carlosbecker.com>
2020-04-05 06:10:39 +0900

    Brew formula update for dockertags version v0.1.4

--- Formula/dockertags.rb
+++ Formula/dockertags.rb
@@ -2,21 +2,21 @@
 class Dockertags < Formula
   desc "docker container tag viewer"
   homepage "https://github.com/goodwithtech/dockertags"
-  version "0.1.3"
+  version "0.1.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/goodwithtech/dockertags/releases/download/v0.1.3/dockertags_0.1.3_macOS-64bit.tar.gz"
-    sha256 "912bb2432480c14426d6dfe2a06d8af7aa4240c5722f0fd427641363132a9164"
+    url "https://github.com/goodwithtech/dockertags/releases/download/v0.1.4/dockertags_0.1.4_macOS-64bit.tar.gz"
+    sha256 "542763536a9313dd3f576faac90bd35c1859ff49aa0a574240bf96e4172bb67e"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/goodwithtech/dockertags/releases/download/v0.1.3/dockertags_0.1.3_Linux-64bit.tar.gz"
-      sha256 "a6f67513e352de3a403afec9df767028e056356c79e1565be5ce8e5547337003"
+      url "https://github.com/goodwithtech/dockertags/releases/download/v0.1.4/dockertags_0.1.4_Linux-64bit.tar.gz"
+      sha256 "38d075086ff2bece199a96cdfb117c95726964c8b55cf017b2a95aaef201d0b7"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/goodwithtech/dockertags/releases/download/v0.1.3/dockertags_0.1.3_Linux-ARM64.tar.gz"
-        sha256 "ab7bf80b0bb3c125ea61d321cc71bbcfd4b0268bc3e9db7ddd836c71b644a034"
+        url "https://github.com/goodwithtech/dockertags/releases/download/v0.1.4/dockertags_0.1.4_Linux-ARM64.tar.gz"
+        sha256 "2f8e90c9c64d11acffb077fa7150c8c5d7208c89624a347f817d43528ed7f76f"
       else
       end
     end
