7fe637c624e0d08bc80366b81609411c313703dc
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-23 15:49:25 +0900

    Brew formula update for dockle version v0.3.12

--- Formula/dockle.rb
+++ Formula/dockle.rb
@@ -5,20 +5,20 @@
 class Dockle < Formula
   desc "Simple security auditing, helping build the Best Docker Images"
   homepage "https://github.com/goodwithtech/dockle"
-  version "0.3.11"
+  version "0.3.12"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/goodwithtech/dockle/releases/download/v0.3.11/dockle_0.3.11_macOS-64bit.tar.gz"
-    sha256 "5a768bd58dc2275a233a54001cd404b26888795148b19c6ad61ee75557da5191"
+    url "https://github.com/goodwithtech/dockle/releases/download/v0.3.12/dockle_0.3.12_macOS-64bit.tar.gz"
+    sha256 "3c8027ad73a67d5c4d2cc6a21b8d5f9df302be712a5a89516d27c6bb5cc77a10"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/goodwithtech/dockle/releases/download/v0.3.11/dockle_0.3.11_Linux-64bit.tar.gz"
-    sha256 "9ff9e4abea964e1c7d211a1bbfb5fc3ad68ec451daec1583cf3f131be3c9a37c"
+    url "https://github.com/goodwithtech/dockle/releases/download/v0.3.12/dockle_0.3.12_Linux-64bit.tar.gz"
+    sha256 "db592faf9833360c8cf609671a08c36887ef80ec8497cbbd472d69d1c71f17f7"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/goodwithtech/dockle/releases/download/v0.3.11/dockle_0.3.11_Linux-ARM64.tar.gz"
-    sha256 "e94ef81534d00d502cb1cdae3b97314012adb1a89e72543cf07808b5c2891980"
+    url "https://github.com/goodwithtech/dockle/releases/download/v0.3.12/dockle_0.3.12_Linux-ARM64.tar.gz"
+    sha256 "dc0500f38a88306b4bc30aef15505c04842747eb96ef6fcf6469ea9fd6777518"
   end
 
   def install
