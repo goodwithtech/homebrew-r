5b34fbe46b6f7e9abcb687ec1f8bd62cc1711523
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-09 01:32:23 +0900

    Brew formula update for dockle version v0.3.14

--- Formula/dockle.rb
+++ Formula/dockle.rb
@@ -5,24 +5,29 @@
 class Dockle < Formula
   desc "Simple security auditing, helping build the Best Docker Images"
   homepage "https://github.com/goodwithtech/dockle"
-  version "0.3.13"
+  version "0.3.14"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/goodwithtech/dockle/releases/download/v0.3.13/dockle_0.3.13_macOS-64bit.tar.gz"
-    sha256 "06f55ee278f8bf2537398692c36a90a3ca300a599cc4ea8323486cf2db9425bc"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/goodwithtech/dockle/releases/download/v0.3.14/dockle_0.3.14_macOS-64bit.tar.gz"
+      sha256 "8c2bd4bb6d0e0b5b1108f4a05b9f71b3ec02fcb3cf58994a7f282fa13654a6eb"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/goodwithtech/dockle/releases/download/v0.3.14/dockle_0.3.14_macOS-ARM64.tar.gz"
+      sha256 "988ea079305aaf695c4937d24997d5ffe44142b3923e3fed087f986ea6dc470d"
+    end
   end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/goodwithtech/dockle/releases/download/v0.3.13/dockle_0.3.13_macOS-ARM64.tar.gz"
-    sha256 "b55daf11c82eeea2c6b8eca24b984aec2125804b27434d81f8e2407deab1b4a0"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/goodwithtech/dockle/releases/download/v0.3.13/dockle_0.3.13_Linux-64bit.tar.gz"
-    sha256 "52595423f77325a833752ea910c6c1d4c9de54296c2f9eab93b3806488bd3d4a"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/goodwithtech/dockle/releases/download/v0.3.13/dockle_0.3.13_Linux-ARM64.tar.gz"
-    sha256 "c3773c0c1eac221f4fb11638e3a8ab2eed29aa6b344887d817dccc8bc7b7e6a3"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/goodwithtech/dockle/releases/download/v0.3.14/dockle_0.3.14_Linux-64bit.tar.gz"
+      sha256 "3168902bcfa49444fdeab916a8bb44cc92fdeaa7c1f0fc71a120328639ec565d"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/goodwithtech/dockle/releases/download/v0.3.14/dockle_0.3.14_Linux-ARM64.tar.gz"
+      sha256 "5d36fe63c159ad26fee2579dbf39e29cadc69953c03f5a148eb190c4f5efd2ec"
+    end
   end
 
   def install
